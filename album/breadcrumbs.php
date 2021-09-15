6cf696d7f1e7d799d4daf531c5e18ef1d88a8ac0
Mauricio Villa <vbamauricio@gmail.com>
2021-09-15 14:07:17 -0300

    Removed param bindging and more detailed category information

--- album/breadcrumbs.php
+++ album/breadcrumbs.php
@@ -1,20 +1,21 @@
 <?php
 // Query categories table
-$categories_query = $db_connection->prepare(
+$categories_result = $db_connection->query(
 	'SELECT p.cid, p.name FROM ' . $categories_table . ' c, ' . $categories_table . ' p
 	WHERE c.lft
 	BETWEEN p.lft
 	AND p.rgt
-	AND c.cid = ?
+	AND c.cid = ' . $main_category . '
 	ORDER BY p.lft'
 );
-$categories_query->bind_param('i', $main_category);
-$categories_query->execute();
-$categories_result = $categories_query->get_result();
 
 // Process each result
 while ($category = $categories_result->fetch_assoc()) {
-	$breadcrumbs[] = $category['name'];
+	$category_data = array();
+	$category_data['id'] = (int)$category['cid'];
+	$category_data['name'] = $category['name'];
+	$category_data['path'] = '/galeria/index.php?cat=' . $category['cid'];
+	$breadcrumbs[] = $category_data;
 }
 
 // Append breadcrumbs' array to the main JSON
